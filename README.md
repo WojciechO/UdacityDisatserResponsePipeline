870d3e7424d8733187387e5656df24846e56e43c
WojciechO <wojciech.orchowski@gmail.com>
2022-01-16 17:08:53 +0000

    Including the code for classifier training, and the pickled model

--- README.md
+++ README.md
@@ -1,14 +1,47 @@
 # Disaster Response Pipeline Project
 
+### Overview
+
+###Prerequisites
+Libraries needed to run the project can be found in /app/requirements file
+
+Most notably, the project requires 
+* scikit-learn with version > 0.20.0 (it is required for ColumnTransaformer)
+* xgboost
+* nltk
+* sqlalchemy
+* plotly
+* flask
+
+
 ### Instructions:
 1. Run the following commands in the project's root directory to set up your database and model.
 
     - To run ETL pipeline that cleans data and stores in database
-        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/disasterPipeline.db`
     - To run ML pipeline that trains classifier and saves
-        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+        `python models/train_classifier.py data/disasterPipeline.db models/classifier.pkl`
+	- Training the XGBoost model may take a while. The already trained model is present in the model/classifier.pkl file
 
 2. Run the following command in the app's directory to run your web app.
     `python run.py`
 
 3. Go to http://0.0.0.0:3001/
+
+### Notes on training and performance
+
+After training and comparing performance of Random Forest classifier and XGBoost classifier, the latter was selected as it improved the recall. XGBoost model was also able to predict occurances of some of the classes with the small support. Random Forest model had 6 classes with low support, that were never predicted. XGB did not predict only 2 classes
+
+Running grid search to tune hyperparameters found the following values to perform the best.
+XGBoost:
+* default parameters
+* learning rate lowered from default 0.3 to 0.2 (learning_rate= 0.2)
+
+TFIDF:
+* including bigrams, apart from unigrams also improved the model (ngram_range= (1, 2))
+
+
+'Genre' column was used as an additional feature. It is one-hot encoded into 3 columns. To enable one-hot encoding on one column and text processing with TFiDF transformation on another, the sklearn ColumnTransformer was used.
+
+For the user input predictions, a default genre 'XX' is provided to the model.
+
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,26 +1,104 @@
+# import libraries
+import pandas as pd
+import numpy as np
 import sys
+from sqlalchemy import create_engine
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import OneHotEncoder
+
+from xgboost import XGBClassifier
+
+import pickle
 
 
 def load_data(database_filepath):
-    pass
+    '''Loading the data from the provided sqlite db. Returns X, y and list of category_names '''
+    
+    url = 'sqlite:///' + database_filepath
+    engine = create_engine(url)
+    df = pd.read_sql_table('messages', engine)
+    X = df[['message', 'genre']]
+    y = df.iloc[:,4:]
+    category_names = list(y.columns)
+
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    '''Cleans, tokenizes and lemmatizes the provided text '''
+    
+    tokenised = word_tokenize(text)
+    lemmatiser = WordNetLemmatizer()
+    
+    normalised = []
+    
+    for token in tokenised:
+        normalised_token = lemmatiser.lemmatize(token).lower().strip()
+        normalised.append(normalised_token)
+        
+    return normalised
 
 
 def build_model():
+    '''Function training the multi classifier model'''
     
+    #defining pipeline for text column transformation:
+    text_transformer = Pipeline(
+        steps =[('vectoriser', CountVectorizer(tokenizer=tokenize)),
+            ('tfidf', TfidfTransformer())
+           ]
+        )
     
-    pass
+    #defining one_hot_encoder, which will be used for 'genre' column
+    one_hot_transformer = OneHotEncoder()
+
+    #defining a column transformer, to use different preprocessing estimators on 'message' and 'genre' columns
+    preprocessor = ColumnTransformer(
+            transformers=[
+                    ("text", text_transformer, 'message'),
+                    ("one_hot", one_hot_transformer, ['genre'])
+                ]
+            )
+
+    #defining the classifier pipeline, using RandomForest as the model
+    classifier = Pipeline(
+                steps=[
+                    ("preprocessor", preprocessor),
+                    ('multiclf', MultiOutputClassifier(XGBClassifier(use_label_encoder=False,
+                                                                     eval_metric = 'logloss')))
+                  ]
+            )
+    
+    return classifier
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''function printing the evaluation score of the model'''
+    y_pred = model.predict(X_test)
+    #turning prediction into data_frame for bette formatting
+    y_pred_df = pd.DataFrame(y_pred, columns = category_names)
+    
+    print(y_pred.shape)
+    print(y_pred_df.shape)
+    for category in category_names:        
+        print(category)
+        print(classification_report(Y_test[category], y_pred_df[category])) 
+        
+    #print(classification_report(Y_test, y_pred_df, target_names = category_names))
 
-
+              
+              
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
@@ -36,19 +114,21 @@ def main():
         print('Training model...')
         model.fit(X_train, Y_train)
         
-        print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
-
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
 
         print('Trained model saved!')
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+
 
     else:
         print('Please provide the filepath of the disaster messages database '\
               'as the first argument and the filepath of the pickle file to '\
               'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+              'train_classifier.py ../data/disasterPipeline.db classifier.pkl')
 
 
 if __name__ == '__main__':
